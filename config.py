f03a4f02c9bea470eba2d549ca23175846636e04
Ben Borszcz <ben.borszcz@gmail.com>
2024-01-16 21:00:30 -0500

    added env var for es pw

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+ELASTICSEARCH_PASSWORD = os.getenv("ELASTICSEARCH_PASSWORD")
 
 DEBUG = True
 ENHANCED_DEBUG = False
