3b13b2ea6d0e7a81e02e07834179ee353208c1ad
Ryan Loney <loney.35@osu.edu>
2024-03-05 12:07:56 -0500

    drop down select for location filter

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -7,7 +7,7 @@ checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
 }
 
 function requestLocation() {
-    if (document.getElementById('distance-slider').value !== "" && navigator.geolocation) {
+    if (document.getElementById('distance-select').value !== "" && navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             // Populate hidden form fields
             document.getElementById('userLat').value = position.coords.latitude;
