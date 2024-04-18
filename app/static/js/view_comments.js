789c39a7e001ed2941cd1265845fa25b37332aa2
Graham Knott <knottgraham@gmail.com>
2024-04-18 12:35:59 -0400

    Small changes throughout the application

--- app/static/js/view_comments.js
+++ app/static/js/view_comments.js
@@ -73,6 +73,17 @@ function loadComments(messages, dealId) {
 }
 
 function createCommentElement (cardBody, comment, data, dealId, parentId) {
+    // Profile picture
+    const img = document.createElement('img');
+    img.className = 'profile-pic-small';
+    img.src = comment.profile_picture ? comment.profile_picture : defaultProfilePic;
+    img.alt = comment.profile_picture ? "User profile picture" : "Default profile picture";
+
+    // {% if comment[0].get('profile_picture') %}
+    //                         <img src="{{ comment[0].get('profile_picture') }}" alt="User profile picture" class="profile-pic-small">
+    //                     {% else %}
+    //                         <img src="{{ url_for('static', filename='images/default_profile.png') }}" alt="Default profile picture" class="profile-pic-small">
+    //                     {% endif %}
     // Username
     const strong = document.createElement('strong');
     strong.textContent = comment.username || 'Unknown User';
@@ -123,6 +134,7 @@ function createCommentElement (cardBody, comment, data, dealId, parentId) {
     data.user_authenticated ? voteDiv.appendChild(downvoteButton) : voteDiv.appendChild(lockedIcon.cloneNode(true));
     
     cardBody.appendChild(voteDiv);
+    cardBody.appendChild(img);
     cardBody.appendChild(p1);
     cardBody.appendChild(p2);
     cardBody.appendChild(small);
